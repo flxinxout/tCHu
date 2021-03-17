597120a9dea326ad8a60c50014cc48bc8d3361c9
Lynoxe <dylan.vairoli@gmail.com>
2021-03-17 15:01:31 +0100

    :recycle: GameState partie 5

--- src/ch/epfl/tchu/game/GameState.java
+++ src/ch/epfl/tchu/game/GameState.java
@@ -1,5 +1,6 @@
 package ch.epfl.tchu.game;
 
+import ch.epfl.tchu.Preconditions;
 import ch.epfl.tchu.SortedBag;
 
 import java.util.*;
@@ -77,7 +78,6 @@ public final class GameState extends PublicGameState {
      */
     @Override
     public PlayerState playerState(PlayerId playerId) {
-        //TODO: cast???
         return (PlayerState) super.playerState(playerId);
     }
 
@@ -87,7 +87,165 @@ public final class GameState extends PublicGameState {
      */
     @Override
     public PlayerState currentPlayerState() {
-        //TODO: cast???
         return (PlayerState) super.currentPlayerState();
+    }
+
+    public SortedBag<Ticket> topTickets(int count){
+        Preconditions.checkArgument(count >= 0 && count <= ticketsCount());
+        return tickets.topCards(count);
+    }
+
+    public GameState withoutTopTickets(int count){
+        Preconditions.checkArgument(count >= 0 && count <= ticketsCount());
+
+        //TODO: better way?
+        PlayerId currentPlayer = currentPlayerId();
+        PlayerId otherPlayer = currentPlayerId().next();
+
+        Map<PlayerId, PlayerState> playerState = new EnumMap<>(PlayerId.class);
+        playerState.put(currentPlayer, currentPlayerState());
+        playerState.put(otherPlayer, playerState(otherPlayer));
+
+        return new GameState(tickets.withoutTopCards(count), cardState(), currentPlayerId(), playerState, lastPlayer());
+    }
+
+    public Card topCard() {
+        Preconditions.checkArgument(!cardState().isDeckEmpty());
+        //TODO: cast? or constructor?
+        CardState cardState = (CardState) cardState();
+        return cardState.topDeckCard();
+    }
+
+    public GameState withoutTopCard(){
+        Preconditions.checkArgument(!cardState().isDeckEmpty());
+        //TODO: better way?
+        PlayerId currentPlayer = currentPlayerId();
+        PlayerId otherPlayer = currentPlayerId().next();
+
+        Map<PlayerId, PlayerState> playerState = new EnumMap<>(PlayerId.class);
+        playerState.put(currentPlayer, currentPlayerState());
+        playerState.put(otherPlayer, playerState(otherPlayer));
+
+        CardState cardState = (CardState) cardState();
+        return new GameState(tickets, cardState.withoutTopDeckCard(), currentPlayerId(), playerState, lastPlayer());
+    }
+
+    public GameState withMoreDiscardedCards(SortedBag<Card> discardedCards){
+        //TODO: better way?
+        PlayerId currentPlayer = currentPlayerId();
+        PlayerId otherPlayer = currentPlayerId().next();
+
+        Map<PlayerId, PlayerState> playerState = new EnumMap<>(PlayerId.class);
+        playerState.put(currentPlayer, currentPlayerState());
+        playerState.put(otherPlayer, playerState(otherPlayer));
+
+        CardState cardState = (CardState) cardState();
+        return new GameState(tickets, cardState.withMoreDiscardedCards(discardedCards), currentPlayerId(), playerState, lastPlayer());
+    }
+
+    public GameState withCardsDeckRecreatedIfNeeded(Random rng){
+        //TODO: better way?
+        PlayerId currentPlayer = currentPlayerId();
+        PlayerId otherPlayer = currentPlayerId().next();
+
+        Map<PlayerId, PlayerState> playerState = new EnumMap<>(PlayerId.class);
+        playerState.put(currentPlayer, currentPlayerState());
+        playerState.put(otherPlayer, playerState(otherPlayer));
+
+        CardState cardState = (CardState) cardState();
+        if (cardState.isDeckEmpty())
+            cardState = cardState.withDeckRecreatedFromDiscards(rng);
+
+        return new GameState(tickets, cardState, currentPlayerId(), playerState, lastPlayer());
+    }
+
+    public GameState withInitiallyChosenTickets(PlayerId playerId, SortedBag<Ticket> chosenTickets){
+        Preconditions.checkArgument(playerState(playerId).tickets().size() == 0);
+
+        //TODO: better way?
+        PlayerId otherPlayer = playerId.next();
+
+        Map<PlayerId, PlayerState> playerState = new EnumMap<>(PlayerId.class);
+        playerState.put(playerId, playerState(playerId).withAddedTickets(chosenTickets));
+        playerState.put(otherPlayer, playerState(otherPlayer));
+
+        return new GameState(tickets, cardState(), currentPlayerId(), playerState, lastPlayer());
+    }
+
+    public GameState withChosenAdditionalTickets(SortedBag<Ticket> drawnTickets, SortedBag<Ticket> chosenTickets){
+        Preconditions.checkArgument(drawnTickets.contains(chosenTickets));
+        //TODO: better way?
+        PlayerId currentPlayer = currentPlayerId();
+        PlayerId otherPlayer = currentPlayerId().next();
+
+        Map<PlayerId, PlayerState> playerState = new EnumMap<>(PlayerId.class);
+        playerState.put(currentPlayer, currentPlayerState().withAddedTickets(chosenTickets));
+        playerState.put(otherPlayer, playerState(otherPlayer));
+
+        return new GameState(tickets.withoutTopCards(drawnTickets.size()), cardState(), currentPlayerId(), playerState, lastPlayer());
+    }
+
+    public GameState withDrawnFaceUpCard(int slot){
+        Preconditions.checkArgument(canDrawCards());
+
+        CardState cardState = (CardState) cardState();
+
+        //TODO: better way?
+        PlayerId currentPlayer = currentPlayerId();
+        PlayerId otherPlayer = currentPlayerId().next();
+
+        Map<PlayerId, PlayerState> playerState = new EnumMap<>(PlayerId.class);
+        playerState.put(currentPlayer, currentPlayerState().withAddedCard(cardState.faceUpCard(slot)));
+        playerState.put(otherPlayer, playerState(otherPlayer));
+
+        return new GameState(tickets, cardState.withDrawnFaceUpCard(slot), currentPlayerId(), playerState, lastPlayer());
+    }
+
+    public GameState withBlindlyDrawnCard(){
+        Preconditions.checkArgument(canDrawCards());
+
+        CardState cardState = (CardState) cardState();
+
+        //TODO: better way?
+        PlayerId currentPlayer = currentPlayerId();
+        PlayerId otherPlayer = currentPlayerId().next();
+
+        Map<PlayerId, PlayerState> playerState = new EnumMap<>(PlayerId.class);
+        playerState.put(currentPlayer, currentPlayerState().withAddedCard(cardState.topDeckCard()));
+        playerState.put(otherPlayer, playerState(otherPlayer));
+
+        return new GameState(tickets, cardState.withoutTopDeckCard(), currentPlayerId(), playerState, lastPlayer());
+    }
+
+    public GameState withClaimedRoute(Route route, SortedBag<Card> cards){
+        //TODO: better way?
+        PlayerId currentPlayer = currentPlayerId();
+        PlayerId otherPlayer = currentPlayerId().next();
+
+        Map<PlayerId, PlayerState> playerState = new EnumMap<>(PlayerId.class);
+        playerState.put(currentPlayer, currentPlayerState().withClaimedRoute(route, cards));
+        playerState.put(otherPlayer, playerState(otherPlayer));
+
+        return new GameState(tickets, cardState(), currentPlayerId(), playerState, lastPlayer());
+    }
+
+    public boolean lastTurnBegins(){
+        return lastPlayer() == null && currentPlayerState().carCount() <= 2;
+    }
+
+    public GameState forNextTurn(){
+        //TODO: better way?
+        PlayerId currentPlayer = currentPlayerId();
+        PlayerId otherPlayer = currentPlayerId().next();
+
+        Map<PlayerId, PlayerState> playerState = new EnumMap<>(PlayerId.class);
+        playerState.put(currentPlayer, currentPlayerState());
+        playerState.put(otherPlayer, playerState(otherPlayer));
+
+        PlayerId lastPlayer = null;
+        if (lastTurnBegins())
+            lastPlayer = currentPlayer;
+
+        return new GameState(tickets, cardState(), currentPlayerId().next(), playerState, lastPlayer);
     }
 }

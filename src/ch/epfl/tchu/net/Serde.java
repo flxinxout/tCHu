5e1dd37dcf99f112767b2ab3555dea95099b0d8b
Lynoxe <dylan.vairoli@gmail.com>
2021-04-13 21:00:19 +0200

    :recycle: lecture rapide

--- src/ch/epfl/tchu/net/Serde.java
+++ src/ch/epfl/tchu/net/Serde.java
@@ -1,12 +1,14 @@
 package ch.epfl.tchu.net;
 
+import ch.epfl.tchu.Preconditions;
 import ch.epfl.tchu.SortedBag;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringJoiner;
 import java.util.function.Function;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * Représente un serializer-deserializer, un objet capable de sérialiser et désérialiser des valeurs d'un type donné.
@@ -33,16 +35,16 @@ public interface Serde<E> {
     E deserialize(String str);
 
     /**
-     * Retourne la Serde correspondant aux fonctions de sérialisation et désérialisation données {@code serialization}
-     * and {@code deserialization}.
+     * Retourne le {@code Serde} correspondant aux fonctions de (dé)sérialisation
+     * {@code serialization} et {@code deserialization}.
      *
      * @param serialization la fonction de sérialisation
      * @param deserialization la fonction de désérialisation
      * @param <T> le paramètre de type de la méthode
-     * @return la serde associée aux 2 fonctions.
+     * @return le serde correspondant aux 2 fonctions
      */
     static <T> Serde<T> of(Function<T, String> serialization, Function<String, T> deserialization) {
-        return new Serde<T>() {
+        return new Serde<>() {
             @Override
             public String serialize(T obj) {
                 return serialization.apply(obj);
@@ -56,65 +58,65 @@ public interface Serde<E> {
     }
 
     /**
-     * Retourne le serde correspondant à la sérialisation et la désérialisation du paramètre donné.
+     * Retourne le {@code Serde} correspondant à la (dé)sérialisation de {@code values} qui représente
+     * la liste de toutes les valeurs d'un ensemble de valeurs énuméré.
      *
-     * @param values les valeurs à sérialiser et désérialiser
+     * @param values les valeurs à sérialiser
      * @param <T> le paramètre de type de la méthode
-     * @return le serde correspondant à la sérialisation et la désérialisation du paramètre donné.
+     * @return le serde correspondant à la sérialisation et la désérialisation de {@code values}
      */
     static <T> Serde<T> oneOf(List<T> values) {
+        Preconditions.checkArgument(!values.isEmpty());
         return Serde.of(i -> Integer.toString(values.indexOf(i)), s -> values.get(Integer.parseInt(s)));
     }
 
     /**
-     * Retourne un serde capable de (dé)sérialiser une liste de valeurs (dé)sérialisées par le serde donné.
+     * Retourne un serde capable de (dé)sérialiser une liste de valeurs (dé)sérialisées par le serde {@code serde}.
      *
      * @param serde le serde donné
-     * @param character le caractère de séparation
+     * @param delimiter le caractère de séparation
      * @param <T> le paramètre de type de la méthode
-     * @return un serde capable de (dé)sérialiser des listes de valeurs (dé)sérialisées par le serde donné.
+     * @return un serde capable de (dé)sérialiser des listes de valeurs (dé)sérialisées par le serde donné
      */
-    static <T> Serde<List<T>> listOf(Serde<T> serde, String character) {
+    static <T> Serde<List<T>> listOf(Serde<T> serde, String delimiter) {
         return new Serde<>() {
             @Override
-            public String serialize(List<T> obj) {
-                StringJoiner join = new StringJoiner(character);
-                obj.forEach(o -> join.add(serde.serialize(o)));
-                return join.toString();
+            public String serialize(List<T> list) {
+                return list.stream()
+                        .map(t -> serde.serialize(t))
+                        .collect(Collectors.joining(delimiter));
             }
 
             @Override
             public List<T> deserialize(String str) {
-                String[] strings = str.split(Pattern.quote(character), -1);
-                final List<T> list = new ArrayList<>();
-                for(String string : strings) {
-                    list.add(serde.deserialize(string));
-                }
-                return list;
+                String[] splits = str.split(Pattern.quote(delimiter), -1);
+                return Arrays.stream(splits)
+                        .map(s -> serde.deserialize(s))
+                        .collect(Collectors.toList());
             }
         };
     }
 
     /**
-     * Retourne un serde capable de (dé)sérialiser un sortedBag de valeurs (dé)sérialisées par le serde donné.
+     * Retourne un serde capable de (dé)sérialiser un {@code SortedBag} de valeurs (dé)sérialisées par le serde donné.
      *
      * @param serde le serde donné
-     * @param character le caractère de séparation
+     * @param separationChar le caractère de séparation
      * @param <T> le paramètre de type de la méthode
      * @return un serde capable de (dé)sérialiser des listes de valeurs (dé)sérialisées par le serde donné.
      */
-    static <T extends Comparable<T>> Serde<SortedBag<T>> bagOf(Serde<T> serde, String character) {
+    static <T extends Comparable<T>> Serde<SortedBag<T>> bagOf(Serde<T> serde, String separationChar) {
         return new Serde<>() {
             @Override
             public String serialize(SortedBag<T> obj) {
-                StringJoiner join = new StringJoiner(character);
+                StringJoiner join = new StringJoiner(separationChar);
                 obj.forEach(o -> join.add(serde.serialize(o)));
                 return join.toString();
             }
 
             @Override
             public SortedBag<T> deserialize(String str) {
-                String[] strings = str.split(Pattern.quote(character), -1);
+                String[] strings = str.split(Pattern.quote(separationChar), -1);
                 final SortedBag.Builder<T> sortedBag = new SortedBag.Builder<>();
                 for(String string : strings) {
                     sortedBag.add(serde.deserialize(string));
@@ -124,27 +126,3 @@ public interface Serde<E> {
         };
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/ch/epfl/tchu/net/Serdes.java
+++ src/ch/epfl/tchu/net/Serdes.java
@@ -20,93 +20,93 @@ public class Serdes {
     }
 
     /**
-     * Serde relative aux entiers.
+     * Serde relatif aux entiers.
      */
     public static final Serde<Integer> SERDE_OF_INTEGERS = Serde.of(i -> Integer.toString(i), Integer::parseInt);
 
     /**
-     * Serde relative aux chaînes de caractères.
+     * Serde relatif aux chaînes de caractères.
      */
     public static final Serde<String> SERDE_OF_STRINGS = Serde.of(
                     s -> Base64.getEncoder().encodeToString(s.getBytes(StandardCharsets.UTF_8)),
                     s -> new String(Base64.getDecoder().decode(s), StandardCharsets.UTF_8));
 
     /**
-     * Serde relative aux PlayerId.
+     * Serde relatif aux PlayerId.
      */
     public static final Serde<PlayerId> SERDE_OF_PLAYER_ID = Serde.oneOf(PlayerId.ALL);
 
     /**
-     * Serde relative aux typex de tour de jeu.
+     * Serde relatif aux typex de tour de jeu.
      */
     public static final Serde<Player.TurnKind> SERDE_OF_TURN_KIND = Serde.oneOf(Player.TurnKind.ALL);
 
     /**
-     * Serde relative aux Card.
+     * Serde relatif aux Card.
      */
     public static final Serde<Card> SERDE_OF_CARDS = Serde.oneOf(Card.ALL);
 
     /**
-     * Serde relative aux routes.
+     * Serde relatif aux routes.
      */
     public static final Serde<Route> SERDE_OF_ROUTES = Serde.oneOf(ChMap.routes());
 
     /**
-     * Serde relative aux tickets.
+     * Serde relatif aux tickets.
      */
     public static final Serde<Ticket> SERDE_OF_TICKETS = Serde.oneOf(ChMap.tickets());
 
     /**
-     * Serde relative aux listes de chaînes de caractères.
+     * Serde relatif aux listes de chaînes de caractères.
      */
     public static final Serde<List<String>> SERDE_OF_LIST_OF_STRINGS = Serde.listOf(SERDE_OF_STRINGS, ",");
 
     /**
-     * Serde relative aux listes de chaînes de caractères.
+     * Serde relatif aux listes de chaînes de caractères.
      */
     public static final Serde<List<Card>> SERDE_OF_LIST_OF_CARDS = Serde.listOf(SERDE_OF_CARDS, ",");
 
     /**
-     * Serde relative aux listes de routes.
+     * Serde relatif aux listes de routes.
      */
     public static final Serde<List<Route>> SERDE_OF_LIST_OF_ROUTES = Serde.listOf(SERDE_OF_ROUTES, ",");
 
     /**
-     * Serde relative aux sorted bag de Card.
+     * Serde relatif aux sorted bag de Card.
      */
     public static final Serde<SortedBag<Card>> SERDE_OF_SORTEGBAG_OF_CARD = Serde.bagOf(SERDE_OF_CARDS, ",");
 
     /**
-     * Serde relative aux sorted bag de tickets.
+     * Serde relatif aux sorted bag de tickets.
      */
     public static final Serde<SortedBag<Ticket>> SERDE_OF_SORTEGBAG_OF_TICKETS = Serde.bagOf(SERDE_OF_TICKETS, ",");
 
     /**
-     * Serde relative aux listes de sorted bag de Card.
+     * Serde relatif aux listes de sorted bag de Card.
      */
     public static final Serde<List<SortedBag<Card>>> SERDE_OF_LIST_OF_SORTEGBAG_OF_CARDS =
             Serde.listOf(Serde.bagOf(SERDE_OF_CARDS, ","), ";");
 
     /**
-     * Serde relative aux PublicCardState.
+     * Serde relatif aux PublicCardState.
      */
     public static final Serde<PublicCardState> SERDE_OF_PUBLIC_CARD_STATE =
             Serde.of(makeFunctionPublicCardState(), makeFunctionStringForPublicCardState());
 
     /**
-     * Serde relative aux PublicPlayerState.
+     * Serde relatif aux PublicPlayerState.
      */
     public static final Serde<PublicPlayerState> SERDE_OF_PUBLIC_PLAYER_STATE =
             Serde.of(makeFunctionPublicPlayerState(), makeFunctionStringForPublicPlayerState());
 
     /**
-     * Serde relative aux PlayerState.
+     * Serde relatif aux PlayerState.
      */
     public static final Serde<PlayerState> SERDE_OF_PLAYER_STATE =
             Serde.of(makeFunctionPlayerState(), makeFunctionStringForPlayerState());
 
     /**
-     * Serde relative aux PublicGameState.
+     * Serde relatif aux PublicGameState.
      */
     public static final Serde<PublicGameState> SERDE_OF_PUBLIC_GAME_STATE =
             Serde.of(makeFunctionPublicGameState(), makeFunctionStringForPublicGameState());
